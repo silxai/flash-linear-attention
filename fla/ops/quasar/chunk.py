afad1bd98b268ce1553744de31bc897a7ee860f5
Quasar Miner <quasar-miner@example.com>
2026-01-18 15:45:18 +0000

    Optimization iteration 1: 124664.19 tokens/sec @ seq_len=100000

--- fla/ops/quasar/chunk.py
+++ fla/ops/quasar/chunk.py
@@ -1,7 +1,6 @@
 import torch
 import triton
 import torch.nn.functional as F
-import triton.language as tl
 from fla.ops.utils.index import prepare_chunk_indices
 from fla.ops.quasar.forward_substitution import forward_substitution_kernel
 from fla.utils import IS_AMD, autocast_custom_bwd, autocast_custom_fwd, autotune_cache_kwargs, check_shared_mem, input_guard
